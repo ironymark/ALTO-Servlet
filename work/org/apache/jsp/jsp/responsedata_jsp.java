314efaca17e41a62f655888dcb8302989dd3b55f
Adil Allawi <adil@diwan.com>
2011-10-11 12:24:20 +0100

    Renamed servlet to alto2 so that it does not clash with older version

--- work/org/apache/jsp/jsp/responsedata_jsp.java
+++ work/org/apache/jsp/jsp/responsedata_jsp.java
@@ -4,19 +4,13 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.jsp.*;
 
-public final class responsedata_jsp extends org.apache.jasper.runtime.HttpJspBase implements org.apache.jasper.runtime.JspSourceDependent {
+public final class responsedata_jsp extends org.apache.jasper.runtime.HttpJspBase
+implements org.apache.jasper.runtime.JspSourceDependent {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = 1L;
-
     private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
-
     private static java.util.List _jspx_dependants;
-
     private org.apache.jasper.runtime.TagHandlerPool _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fescapeXml_005fnobody;
-
     private javax.el.ExpressionFactory _el_expressionfactory;
     private org.apache.AnnotationProcessor _jsp_annotationprocessor;
 
@@ -25,18 +19,17 @@ public final class responsedata_jsp extends org.apache.jasper.runtime.HttpJspBas
     }
 
     public void _jspInit() {
-        _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fescapeXml_005fnobody = org.apache.jasper.runtime.TagHandlerPool
-                .getTagHandlerPool(getServletConfig());
+        _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fescapeXml_005fnobody = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
         _el_expressionfactory = _jspxFactory.getJspApplicationContext(getServletConfig().getServletContext()).getExpressionFactory();
-        _jsp_annotationprocessor = (org.apache.AnnotationProcessor) getServletConfig().getServletContext().getAttribute(
-                org.apache.AnnotationProcessor.class.getName());
+        _jsp_annotationprocessor = (org.apache.AnnotationProcessor) getServletConfig().getServletContext().getAttribute(org.apache.AnnotationProcessor.class.getName());
     }
 
     public void _jspDestroy() {
         _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fescapeXml_005fnobody.release();
     }
 
-    public void _jspService(HttpServletRequest request, HttpServletResponse response) throws java.io.IOException, ServletException {
+    public void _jspService(HttpServletRequest request, HttpServletResponse response)
+            throws java.io.IOException, ServletException {
 
         PageContext pageContext = null;
         HttpSession session = null;
@@ -47,9 +40,11 @@ public final class responsedata_jsp extends org.apache.jasper.runtime.HttpJspBas
         JspWriter _jspx_out = null;
         PageContext _jspx_page_context = null;
 
+
         try {
             response.setContentType("text/javascript;charset=UTF-8");
-            pageContext = _jspxFactory.getPageContext(this, request, response, null, true, 8192, true);
+            pageContext = _jspxFactory.getPageContext(this, request, response,
+                    null, true, 8192, true);
             _jspx_page_context = pageContext;
             application = pageContext.getServletContext();
             config = pageContext.getServletConfig();
@@ -60,37 +55,28 @@ public final class responsedata_jsp extends org.apache.jasper.runtime.HttpJspBas
             if (_jspx_meth_c_005fout_005f0(_jspx_page_context))
                 return;
         } catch (Throwable t) {
-            if (!(t instanceof SkipPageException)) {
+            if (!(t instanceof SkipPageException)){
                 out = _jspx_out;
                 if (out != null && out.getBufferSize() != 0)
-                    try {
-                        out.clearBuffer();
-                    } catch (java.io.IOException e) {
-                    }
-                if (_jspx_page_context != null)
-                    _jspx_page_context.handlePageException(t);
+                    try { out.clearBuffer(); } catch (java.io.IOException e) {}
+                if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
             }
         } finally {
             _jspxFactory.releasePageContext(_jspx_page_context);
         }
     }
 
-    private boolean _jspx_meth_c_005fout_005f0(PageContext _jspx_page_context) throws Throwable {
+    private boolean _jspx_meth_c_005fout_005f0(PageContext _jspx_page_context)
+            throws Throwable {
         PageContext pageContext = _jspx_page_context;
         JspWriter out = _jspx_page_context.getOut();
-        // c:out
-        org.apache.taglibs.standard.tag.rt.core.OutTag _jspx_th_c_005fout_005f0 = (org.apache.taglibs.standard.tag.rt.core.OutTag) _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fescapeXml_005fnobody
-                .get(org.apache.taglibs.standard.tag.rt.core.OutTag.class);
+        //  c:out
+        org.apache.taglibs.standard.tag.rt.core.OutTag _jspx_th_c_005fout_005f0 = (org.apache.taglibs.standard.tag.rt.core.OutTag) _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fescapeXml_005fnobody.get(org.apache.taglibs.standard.tag.rt.core.OutTag.class);
         _jspx_th_c_005fout_005f0.setPageContext(_jspx_page_context);
         _jspx_th_c_005fout_005f0.setParent(null);
-        // /jsp/responsedata.jsp(1,199) name = value type = null reqTime = true
-        // required = true fragment = false deferredValue = false
-        // expectedTypeName = null deferredMethod = false methodSignature = null
-        _jspx_th_c_005fout_005f0.setValue((java.lang.Object) org.apache.jasper.runtime.PageContextImpl.proprietaryEvaluate("${respData}",
-                java.lang.Object.class, (PageContext) _jspx_page_context, null, false));
-        // /jsp/responsedata.jsp(1,199) name = escapeXml type = null reqTime =
-        // true required = false fragment = false deferredValue = false
-        // expectedTypeName = null deferredMethod = false methodSignature = null
+        // /jsp/responsedata.jsp(1,199) name = value type = null reqTime = true required = true fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+        _jspx_th_c_005fout_005f0.setValue((java.lang.Object) org.apache.jasper.runtime.PageContextImpl.proprietaryEvaluate("${respData}", java.lang.Object.class, (PageContext)_jspx_page_context, null, false));
+        // /jsp/responsedata.jsp(1,199) name = escapeXml type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
         _jspx_th_c_005fout_005f0.setEscapeXml(false);
         int _jspx_eval_c_005fout_005f0 = _jspx_th_c_005fout_005f0.doStartTag();
         if (_jspx_th_c_005fout_005f0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
